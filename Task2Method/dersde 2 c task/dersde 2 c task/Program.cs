0dc8e7e593e581ee876e4ea835fcc589bc13d722
BehruzRehimli <121186387+BehruzRehimli@users.noreply.github.com>
2023-01-17 23:25:33 +0400

    Add files via upload

---
+++ Task2Method/dersde 2 c task/dersde 2 c task/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+
+namespace dersde_2_c_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //Console.WriteLine("Zehmet olmasa n ededini daxil edin");
+            //int n= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Zehmet olmasa m ededini daxil edin");
+            //int m= Convert.ToInt32(Console.ReadLine());
+            //int bigger;
+            //int lower;
+            //int sum = 0;
+            //double counter= 0; 
+            //if (n>m)
+            //{
+            //    bigger = n;
+            //    lower = m;
+
+            //}
+
+            //else
+            //{
+            //    bigger = m;
+            //    lower = n;
+
+            //}
+            //for (int i = lower; i <= bigger; i++)
+            //{
+            //    if (i % 21 == 0)
+            //    {
+            //        sum += i;
+            //        counter++;
+            //    }
+            //}
+            //Console.WriteLine(sum / counter);
+
+            #endregion
+            #region Task 2
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //while (n >= 1)
+            //{
+            //    sum += n % 10;
+            //    n /= 10;
+            //}
+
+
+            //Console.WriteLine(sum);
+            #endregion
+            #region Task 3
+            //Console.WriteLine("Verileni daxil edin");
+            //string input=Console.ReadLine();
+            //bool inputSearch = false;
+            //for (int i = 0; i < input.Length; i++)
+            //{
+            //    if (input[i]=='1')
+            //    {                    
+            //        inputSearch = true;
+            //        break;
+            //    }
+
+            //}
+            //if (inputSearch==true)
+            //{
+            //    Console.WriteLine("Var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yoxdur");
+            //}
+            #endregion
+
+        }
+    }
+}
